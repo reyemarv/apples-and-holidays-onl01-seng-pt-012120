17e428372aac700f0e7ca94939ee0b011b91c5f6
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 03:00:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,15 +31,17 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do | season, holiday|
      puts "#{season.capitalize}:"
      holiday.each do |holiday, item|
-       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+       puts "  #{holiday.to_s.split("_").collect {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
   end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+    holiday_hash.collect do |season, holiday|
+    holiday.collect do |holiday, item|
+      holiday if item.include?("BBQ")
+    end 
+  end 
 end
 
 
