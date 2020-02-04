880000f4a0971402203ea81febc13571f9a79169
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 02:58:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,13 +28,12 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
-    holiday_hash.each do |holiday, item|
-      puts "#{holiday.to_s.split("_").collect {|x| x.capitalize}.join(" ")}: #{item.join(", ")}"
-    end 
-    end
-
+holiday_hash.each do | season, holiday|
+     puts "#{season.capitalize}:"
+     holiday.each do |holiday, item|
+       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+     end
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
