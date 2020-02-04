44dbd5713475747f9b4fe376bfd64293a209160f
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 02:37:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -5,7 +5,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_has[:winter].each do |holiday, 
+  holiday_hash[:winter].each do |holiday, decorations|
+    decorations << items
+  end 
 
 end
 
